db009bcc96aad8d4d492d2469692ffa0ff280d72
Parag-dwn <paragdewangan1@gmail.com>
2022-05-02 04:35:35 +0000

    test1.java

--- test1.java
+++ test1.java
@@ -1,7 +1,32 @@
-System.out.println("Funciton 1: version 1:");
-class main(){
-func1(){
-println(this is first funciton")
-}
+[33mcommit ac9dde1d3b8965be4f422533c0aa894d7e148c37[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Parag-dwn <paragdewangan1@gmail.com>
+Date:   Mon May 2 04:16:43 2022 +0000
 
-}
+    test1.java
+
+[1mdiff --git a/test1.java b/test1.java[m
+[1mindex e69de29..e3d99f1 100644[m
+[1m--- a/test1.java[m
+[1m+++ b/test1.java[m
+[36m@@ -0,0 +1,7 @@[m
+[32m+[m[32mSystem.out.println("Funciton 1: version 1:");[m
+[32m+[m[32mclass main(){[m
+[32m+[m[32mfunc1(){[m
+[32m+[m[32mprintln(this is first funciton")[m
+[32m+[m[32m}[m
+[32m+[m
+[32m+[m[32m}[m
+
+[33mcommit 4b628b03c19b20207ba0e6e57343879c94656550[m[33m ([m[1;33mtag: v1.0[m[33m)[m
+Author: Parag-dwn <paragdewangan1@gmail.com>
+Date:   Mon Apr 18 05:43:48 2022 +0000
+
+    new file created
+
+[1mdiff --git a/test1.java b/test1.java[m
+[1mnew file mode 100644[m
+
+[1mindex 0000000..e69de29[m
+
+funciton2: this is update2:
+
